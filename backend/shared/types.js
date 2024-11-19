a87ee27ba32038c9894aae5ea50faf19c0ac8189
Toivo Jija <rayman01@bk.ru>
2024-11-19 14:27:14 +0800

    cleanup

--- backend/shared/types.js
+++
@@ -1,7 +0,0 @@
-export const Actions = {
-    addRemainder: "add_remainder",
-    increaseRemainder: "increase_remainder",
-    decreaseRemainder: "decrease_remainder",
-    placeOrder: "place_order",
-    resolveOrder: "resolve_order"
-}
--- backend/src/serviceTwo/routes/app.routes.js
+++
@@ -1,8 +0,0 @@
-import { app } from ".."
-import { setupHistoryRoutes } from "./history.routes"
-/**
- * @param {typeof app} app 
- */
-export const setupAppRoutes = (app) => {
-    setupHistoryRoutes(app);
-}
